b217c2e36358004bd2302b95831a3bc1b8cc6268
LS Hung <61226932+lokshunhung@users.noreply.github.com>
2020-07-27 19:16:00 +0800

    [REFACTAOR]: app: Use ws eslintrc & fix lint

--- apps/app/__tests__/App-test.tsx
+++ apps/app/__tests__/App-test.tsx
@@ -1,10 +1,12 @@
 import 'react-native';
-import React from 'react';
 import { App } from '@lsh/core/src/App';
-
+import React from 'react';
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
 
-it('renders correctly', () => {
-    renderer.create(<App />);
+describe('App', () => {
+    test('renders correctly', () => {
+        expect.assertions(0);
+        renderer.create(<App />);
+    });
 });
