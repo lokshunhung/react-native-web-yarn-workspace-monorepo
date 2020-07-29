9c8ef7d21be5de67f8aab64ba6e2d80dbe6c86b3
LS Hung <61226932+lokshunhung@users.noreply.github.com>
2020-07-29 10:20:37 +0800

    [CHORE]: app: Document use of projectRoot in metro config

--- apps/app/metro.config.js
+++ apps/app/metro.config.js
@@ -4,6 +4,7 @@ const path = require('path');
 
 /** @type { import('./types/metro/metro-config').MetroConfig.InputConfigT } */
 const config = {
+    // https://github.com/facebook/metro/blob/38fa02d88fd66c150359c46ba0b5bb0eb536bbac/packages/metro-config/src/defaults/index.js#L127
     projectRoot: path.join(__dirname, '..', '..'),
     transformer: {
         getTransformOptions: async (entryPoints, options, getDependenciesOf) => ({
